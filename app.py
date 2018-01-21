ab5184967664e9fee7b6a70d957f4439c3bb2496
pan chong <lspanchong@gmail.com>
2018-01-21 21:06:56 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,93 @@
+from flask import Flask, jsonify
+from flask import abort
+from flask import make_response
+from flask import request
+from flask import url_for
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': 'Buy groceries',
+        'description': 'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': 'Learn Python',
+        'description': 'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': list(map(make_public_task, tasks))})
+
+
+@app.route('/todo/api/v1.0/task/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = list(filter(lambda t: t['id'] == task_id, tasks))
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not ('title' in request.json):
+        abort(404)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ''),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    print(task_id)
+    task = list(filter(lambda t: t['id'] == task_id, tasks))
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(404)
+
+    tasks[0]['title'] = request.json.get('title', tasks[0]['title'])
+    tasks[0]['description'] = request.json.get('description', tasks[0]['description'])
+    tasks[0]['done'] = request.json.get('done', tasks[0]['done'])
+    return jsonify({'task': tasks[0]})
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = list(filter(lambda t: t['id'] == task_id, tasks))
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
+
+@app.errorhandler(404)
+def not_found(error):
+    print(type(error))
+    return make_response(jsonify({'error': 'Not found', "desc": error.description}))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
